be5b53166afc6087f11e239c8bf0549593e5701f
rich <rich.p.martin@gmail.com>
2022-05-12 12:58:22 -0700

    clean up

--- 01_source_me.R
+++ 01_source_me.R
@@ -1,16 +1,30 @@
-# Copyright 2022 Province of British Columbia
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and limitations under the License.
 library(tidyverse)
-source(here::here("R","functions.R"))
+bins <- 10
+
+regional_districts <- sf::st_read(here::here(
+  "map_data",
+  "Regional Districts",
+  "ABMS_RD_polygon.shp"
+)) %>%
+  janitor::clean_names() %>%
+  select(aa_name, geometry)
+one_large_municipality <- sf::st_read(here::here(
+  "map_data",
+  "Municipalities",
+  "ABMS_MUNI_polygon.shp"
+)) %>%
+  janitor::clean_names() %>%
+  filter(area_sqm == max(area_sqm)) %>%
+  select(aa_name, geometry)
+regional_districts <- bind_rows(regional_districts, one_large_municipality) %>%
+  mutate(
+    aa_name = janitor::make_clean_names(aa_name),
+    aa_name = str_replace(aa_name, "regional_district_of_", ""),
+    aa_name = str_replace(aa_name, "_regional_district", ""),
+    aa_name = str_replace(aa_name, "_region_unincorporated", ""),
+    aa_name = str_replace(aa_name, "_regional_municipality", "")
+  )
+
 column_names <- c(
   "Region",
   "District",
@@ -22,6 +36,7 @@ column_names <- c(
   "Job Postings: per capita",
   "MPI Estimated Cost: raw data",
   "MPI Estimated Cost: per 1,000 capita",
+  "Small Business Registrations: raw data",
   "Population 2020: raw data",
   "Population 2021: raw data",
   "Population: growth",
@@ -31,63 +46,75 @@ column_names <- c(
   "Building Permits: index",
   "Job Postings: index",
   "MPI Estimated Cost: index",
+  "Small Business Registration: index",
   "Population: index",
   "Overall: index"
 )
 district_conditions <- readxl::read_excel(here::here("raw_data", "2022 update data.xlsx"),
-                                          range = "B6:V42",
-                                          col_names = column_names,
-                                          na = c("n/a","n.a.","n.a"))%>%
-  select(-x1,-x2)%>%
-  fill(Region)%>%
-  select(Region,
-         District,
-         `Overall: index`,
-         contains("index"),
-         contains("capita"),
-         contains("growth"),
-         everything())%>%
-  filter(!is.na(District))%>%
-  mutate(District=janitor::make_clean_names(District),
-         District=str_replace(District, "greater_vancouver", "metro_vancouver"),
-         District=str_replace(District, "powell_river", "qathet"),
-         District=str_replace(District, "skeena_queen_charlotte", "north_coast")
-  )%>%
+  sheet = "RD Current Conditions Monitor",
+  range = "B6:X42",
+  col_names = column_names,
+  na = c("n/a", "n.a.", "n.a")
+) %>%
+  select(-x1, -x2) %>%
+  fill(Region) %>%
+  filter(!is.na(District)) %>%
+  mutate(
+    District = janitor::make_clean_names(District),
+    District = str_replace(District, "greater_vancouver", "metro_vancouver"),
+    District = str_replace(District, "powell_river", "qathet"),
+    District = str_replace(District, "skeena_queen_charlotte", "north_coast")
+  ) %>%
+  select(Region, District, ends_with("raw data")) %>%
+  mutate(
+    `Income Assistance: per capita` = `Income Assistance: raw data` / `Population 2021: raw data`,
+    `Building Permits: per capita` = `Building Permits: raw data` / (`Population 2021: raw data`),
+    `Job Postings: per capita` = `Job Postings: raw data` / `Population 2021: raw data`,
+    `MPI Estimated Cost: per capita` = `MPI Estimated Cost: raw data` / (`Population 2021: raw data`),
+    `Small Business Registrations: per capita` = `Small Business Registrations: raw data` / (`Population 2021: raw data`),
+    `Population: growth` = `Population 2021: raw data` / `Population 2020: raw data` - 1
+  )
+
+pca <- district_conditions %>%
+  select(District, contains("per capita"), contains("growth")) %>%
+  column_to_rownames(var = "District") %>%
+  as.matrix() %>%
+  nipals::nipals(fitted = TRUE)
+
+class(pca) <- "princomp"
+pca$sdev <- sqrt(pca$eig)
+pca$n.obs <- dim(pca$scores)[1]
+
+pca_index <- as_tibble(pca$scores, rownames = "district") %>%
+  select(district, `PCA: index` = PC1)
+
+district_conditions <- full_join(district_conditions, pca_index, by = c("District" = "district"))%>%
+  mutate(
+    `Income Assistance: decile` = ntile(-`Income Assistance: per capita`, bins), # negative because lower is better.
+    `Building Permits: decile` = ntile(`Building Permits: per capita`, bins),
+    `Job Postings: decile` = ntile(`Job Postings: per capita`, bins),
+    `MPI Estimated Cost: decile` = ntile(`MPI Estimated Cost: per capita`, bins),
+    `Small Business Registrations: decile` = ntile(`Small Business Registrations: per capita`, bins),
+    `Population: decile` = ntile(`Population: growth`, bins)
+  ) %>%
+  rowwise() %>%
+  mutate(`Overall: index` = mean(c(
+    `Income Assistance: decile`,
+    `Building Permits: decile`,
+    `Job Postings: decile`,
+    `MPI Estimated Cost: decile`,
+    `Small Business Registrations: decile`,
+    `Population: decile`
+  ), na.rm = TRUE)) %>%
   pivot_longer(cols = -c(Region, District), names_to = "name", values_to = "value")%>%
-  janitor::clean_names()%>%
-  mutate(name2 = name)%>%
-  separate(name2, into = c("thing", "type"), sep = ": ")%>%
-  mutate(type = str_replace_all(type, "per capita", "normalized"),
-         type = str_replace_all(type, "per 1,000 capita", "normalized"),
-         type = str_replace_all(type, "growth", "normalized")
-  )
+  full_join(regional_districts, by = c("District" = "aa_name")) %>%
+  na.omit()%>%
+  mutate(name2=name,
+         District= str_to_title(str_replace_all(District, "_", " ")))%>%
+  separate(name2, into=c("thing", "type"), sep = ": ")%>%
+  mutate(type=case_when(type=="per capita"~"normalized",
+                        type=="growth"~"normalized",
+                        TRUE~type))
 
-
-regional_districts <- sf::st_read(here::here("map_data",
-                                             "Regional Districts",
-                                             "ABMS_RD_polygon.shp"))%>%
-  janitor::clean_names()%>%
-  select(aa_name, geometry)
-one_large_municipality <- sf::st_read(here::here("map_data",
-                                                 "Municipalities",
-                                                 "ABMS_MUNI_polygon.shp"))%>%
-  janitor::clean_names()%>%
-  filter(area_sqm == max(area_sqm))%>%
-  select(aa_name, geometry)
-regional_districts <- bind_rows(regional_districts, one_large_municipality)%>%
-  mutate(aa_name = janitor::make_clean_names(aa_name),
-         aa_name = str_replace(aa_name, "regional_district_of_", ""),
-         aa_name = str_replace(aa_name, "_regional_district", ""),
-         aa_name = str_replace(aa_name, "_region_unincorporated", ""),
-         aa_name = str_replace(aa_name, "_regional_municipality", "")
-  )
-
-
-df <- full_join(regional_districts, district_conditions, by=c("aa_name"="district"))
-
-choropleths <- tibble(name=unique(district_conditions$name))%>%
-  filter(!grepl('raw data', name))%>%
-  mutate(the_map=map(name, static_map, df))
-
-saveRDS(choropleths, here::here("processed_data", "choropleths.rds"), compress = FALSE)
-saveRDS(district_conditions, here::here("processed_data", "district_conditions.rds"), compress = FALSE)
+saveRDS(district_conditions,here::here("processed_data","district_conditions.rds"))
+saveRDS(pca,here::here("processed_data","pca.rds"))
--- R/functions.R
+++ R/functions.R
@@ -1,22 +1,44 @@
-static_map <- function(nm, df){
+static_map <- function(nm, tbbl, bc_map, anns) {
+  set.seed(123)
   print(nm)
-  df <- df%>%
-    filter(name==nm)
-  label_colour = ifelse(df$value< mean(df$value, na.rm=TRUE), "white", "black")
-  plt <-  ggplot(data = df, mapping=aes(geometry = geometry,
-                                        fill=value,
-                                        label= paste0(
-                                        str_to_title(str_replace_all(aa_name,"_"," ")),
-                                          ": ",
-                                        scales::comma(value, accuracy = .01))))+
-    geom_sf(lwd=.2, colour="white")+
-    ggrepel::geom_label_repel(colour=label_colour, stat = "sf_coordinates", size=3.5, min.segment.length = 1)+
-    scale_fill_viridis_c(label=scales::comma, name = "", na.value="grey90")+
-    theme_void()+
-    theme(plot.margin = margin(t = 0,  # Top margin
-                               r = 0,  # Right margin
-                               b = 0,  # Bottom margin
-                               l = 0)) # Left margin
-
+  tbbl <- tbbl %>%
+    filter(name == nm)
+  label_colour <- ifelse(tbbl$value < mean(tbbl$value, na.rm = TRUE), "white", "black")
+  plt <- ggplot() +
+    geom_sf(data = tbbl, mapping = aes(
+      geometry = geometry,
+      fill = value
+    ), lwd = 0.5, colour = "white") +
+    coord_sf(expand = FALSE)
+    if(anns==TRUE){
+      plt <- plt+
+        geom_sf(data = bc_map, colour = "black", fill='brown', alpha=.05)
+    }
+    plt <- plt+
+      ggrepel::geom_label_repel(data = tbbl, mapping = aes(
+      geometry = geometry,
+      fill = value,
+      label = paste0(
+        str_to_title(str_replace_all(District, "_", " ")),
+        ": ",
+        scales::comma(value, accuracy = .01)
+      )
+    ), colour = label_colour, stat = "sf_coordinates", size = 5, min.segment.length = 1) +
+    scale_fill_viridis_c(label = scales::comma, name = "", na.value = 'white')
+    if(anns == TRUE){
+      plt <- plt+
+    ggspatial::annotation_scale(location = "bl", width_hint = 0.5) +
+    ggspatial::annotation_north_arrow(location = "bl",
+                           which_north = "true",
+                           pad_x = unit(0.75, "in"),
+                           pad_y = unit(0.5, "in")) +
+    xlab("") +
+    ylab("") +
+    theme(panel.grid.major = element_line(color = gray(.5),
+                                          linetype = "dashed", size = 0.5))
+    }else{
+      plt <- plt+
+        ggthemes::theme_map()
+    }
   aest::aest_fix_labs(plt)
 }
