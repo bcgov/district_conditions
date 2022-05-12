7b712e2eca1c602e38522e1f934bf1b36176f4ee
rich <rich.p.martin@gmail.com>
2022-05-12 13:47:05 -0700

    spring cleaning

--- 01_source_me.R
+++ 01_source_me.R
@@ -1,6 +1,9 @@
 library(tidyverse)
-bins <- 10
-
+#One way to create an index based on several economic indicators is to bin the data e.g. deciles and then create the index as an average
+#of the decile scores for each indicator.
+bins <- 10 # decile scores.
+#the shape data for maps---------
+#mostly regional districts---------
 regional_districts <- sf::st_read(here::here(
   "map_data",
   "Regional Districts",
@@ -8,6 +11,7 @@ regional_districts <- sf::st_read(here::here(
 )) %>%
   janitor::clean_names() %>%
   select(aa_name, geometry)
+#plus one large municipality-------
 one_large_municipality <- sf::st_read(here::here(
   "map_data",
   "Municipalities",
@@ -16,6 +20,7 @@ one_large_municipality <- sf::st_read(here::here(
   janitor::clean_names() %>%
   filter(area_sqm == max(area_sqm)) %>%
   select(aa_name, geometry)
+#bind the regional districts and one large municipality together
 regional_districts <- bind_rows(regional_districts, one_large_municipality) %>%
   mutate(
     aa_name = janitor::make_clean_names(aa_name),
@@ -24,7 +29,8 @@ regional_districts <- bind_rows(regional_districts, one_large_municipality) %>%
     aa_name = str_replace(aa_name, "_region_unincorporated", ""),
     aa_name = str_replace(aa_name, "_regional_municipality", "")
   )
-
+#the excel file features non-unique column names, and missing names (for empty columns)
+#create column names (this could easily break if the file structure changes)
 column_names <- c(
   "Region",
   "District",
@@ -50,6 +56,7 @@ column_names <- c(
   "Population: index",
   "Overall: index"
 )
+#read the excel file.  This could break if structure of file changes. e.g. reordering columns, additional columns, etc.
 district_conditions <- readxl::read_excel(here::here("raw_data", "2022 update data.xlsx"),
   sheet = "RD Current Conditions Monitor",
   range = "B6:X42",
@@ -59,6 +66,7 @@ district_conditions <- readxl::read_excel(here::here("raw_data", "2022 update da
   select(-x1, -x2) %>%
   fill(Region) %>%
   filter(!is.na(District)) %>%
+  #modify names so they  match up with the spatial data-------
   mutate(
     District = janitor::make_clean_names(District),
     District = str_replace(District, "greater_vancouver", "metro_vancouver"),
@@ -66,6 +74,7 @@ district_conditions <- readxl::read_excel(here::here("raw_data", "2022 update da
     District = str_replace(District, "skeena_queen_charlotte", "north_coast")
   ) %>%
   select(Region, District, ends_with("raw data")) %>%
+  #calculate the normalized measures.-----------
   mutate(
     `Income Assistance: per capita` = `Income Assistance: raw data` / `Population 2021: raw data`,
     `Building Permits: per capita` = `Building Permits: raw data` / (`Population 2021: raw data`),
@@ -74,21 +83,21 @@ district_conditions <- readxl::read_excel(here::here("raw_data", "2022 update da
     `Small Business Registrations: per capita` = `Small Business Registrations: raw data` / (`Population 2021: raw data`),
     `Population: growth` = `Population 2021: raw data` / `Population 2020: raw data` - 1
   )
-
+# An alternative way of creating an index based on multiple measures is to perform principal component analysis, where the first
+# principal component serves as the index.
+# Here we use Nipals because it can deal with missing volues.
 pca <- district_conditions %>%
   select(District, contains("per capita"), contains("growth")) %>%
   column_to_rownames(var = "District") %>%
   as.matrix() %>%
   nipals::nipals(fitted = TRUE)
-
 class(pca) <- "princomp"
 pca$sdev <- sqrt(pca$eig)
 pca$n.obs <- dim(pca$scores)[1]
-
 pca_index <- as_tibble(pca$scores, rownames = "district") %>%
   select(district, `PCA: index` = PC1)
-
 district_conditions <- full_join(district_conditions, pca_index, by = c("District" = "district"))%>%
+  #create the decile scores-----------
   mutate(
     `Income Assistance: decile` = ntile(-`Income Assistance: per capita`, bins), # negative because lower is better.
     `Building Permits: decile` = ntile(`Building Permits: per capita`, bins),
@@ -98,6 +107,7 @@ district_conditions <- full_join(district_conditions, pca_index, by = c("Distric
     `Population: decile` = ntile(`Population: growth`, bins)
   ) %>%
   rowwise() %>%
+  #overall index is the mean of the AVAILABLE decile scores (implicitly assuming missing at random)
   mutate(`Overall: index` = mean(c(
     `Income Assistance: decile`,
     `Building Permits: decile`,
@@ -108,6 +118,7 @@ district_conditions <- full_join(district_conditions, pca_index, by = c("Distric
   ), na.rm = TRUE)) %>%
   pivot_longer(cols = -c(Region, District), names_to = "name", values_to = "value")%>%
   full_join(regional_districts, by = c("District" = "aa_name")) %>%
+  #removing the missing data important for map labeling.---------
   na.omit()%>%
   mutate(name2=name,
          District= str_to_title(str_replace_all(District, "_", " ")))%>%
@@ -115,6 +126,17 @@ district_conditions <- full_join(district_conditions, pca_index, by = c("Distric
   mutate(type=case_when(type=="per capita"~"normalized",
                         type=="growth"~"normalized",
                         TRUE~type))
+#for the district plot we need calculate the proportion of the maximal value---------
+standardized_vars <- unique(district_conditions$name)[
+  str_detect(unique(district_conditions$name), pattern="per capita") |
+    str_detect(unique(district_conditions$name), pattern="growth")]
+standardized <- district_conditions%>%
+  select(-geometry)%>%
+  filter(name %in% standardized_vars)%>%
+  group_by(name)%>%
+  mutate(prop_of_max = value/max(value, na.rm=TRUE))
 
-saveRDS(district_conditions,here::here("processed_data","district_conditions.rds"))
-saveRDS(pca,here::here("processed_data","pca.rds"))
+#save the data for the app.----
+saveRDS(district_conditions, here::here("processed_data", "district_conditions.rds"))
+saveRDS(standardized, here::here("processed_data", "standardized.rds"))
+saveRDS(pca,here::here("processed_data", "pca.rds"))
